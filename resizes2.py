7299e47ea1c2efbcea0356e15f42e2b8f15c230a
Toulousegg <franklin.m.mogollon@gmail.com>
2021-12-07 00:30:17 -0400

    cambio while a for y nota

--- resizes2.py
+++ resizes2.py
@@ -7,6 +7,9 @@ direccion = 'D:/proyectos/ayuda skinning/Images'
 images = os.listdir(direccion)
 #print(images)
 
+# Nota de lo que aprendí: los bucles sirven para realizar una tarea una y otra vez hasta que se le diga que pare por una condición (las condiciones
+# pueden ser tanto 'BREAK' como 'CONTINUE', el break termina por completo el bucle, el continue solo ignora tal cosa, puede ser un bucle While o bucle for
+# las dos son muy parecidas y sinceramente no conozco que diferencias tendrán entre ellas.#
 for imagen_name in images:
     imagen_to_resize = Image.open(direccion + images)
     ancho, alto = Image.size
