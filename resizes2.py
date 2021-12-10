f329cd694aa8ae4dfc5bbf2aeafb9df751ed131d
Toulousegg <franklin.m.mogollon@gmail.com>
2021-12-10 06:52:04 -0400

    rename a las variables y uso de nueva función (linea 14-15)

--- resizes2.py
+++ resizes2.py
@@ -4,13 +4,15 @@ import pathlib
 import cv2
 
 direccion = 'D:/proyectos/ayuda skinning/Images'
-images = os.listdir(direccion)
+imagen_name = os.listdir(direccion)
 
 # Nota de lo que aprendí: los bucles sirven para realizar una tarea una y otra vez hasta que se le diga que pare por una condición (las condiciones
 # pueden ser tanto 'BREAK' como 'CONTINUE', el break termina por completo el bucle, el continue solo ignora tal cosa, puede ser un bucle While o bucle for
 # las dos son muy parecidas y sinceramente no conozco que diferencias tendrán entre ellas.#
-for imagen_name in images:
-    imagen_to_resize = Image.open(direccion ,images, {repr ('r')})
+
+for imagen_name in imagen_name:
+    image = os.path.join(direccion, imagen_name)
+    imagen_to_resize = Image.open(image, 'r')
     ancho, alto = Image.size
     Image.size = Image.resize((ancho/2, alto/2))
     #GUARDADO
@@ -19,5 +21,5 @@ for imagen_name in images:
 #Esto es para que cuando quiera redimensionar más de una imagen cambie su nombre
     #if images
 
-    if '@2x' not in images:
+    if '@2x' not in imagen_name:
         break
