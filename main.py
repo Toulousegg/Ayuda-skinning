30531b57e758c68c37ec21b261a56111eacfbd2e
Toulousegg <franklin.m.mogollon@gmail.com>
2022-02-02 17:28:31 -0300

    Empiezo a hacer interfaz

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import tk, label, Button
+from tkinter.ttk import Label
+
+class InterfazGrafica:
+    def __init__(self, master):
+        self.master = master
+        master.title('Resizes Imagen')
+
+        self.etiqueta = Label(master, text='Say me how much images do you gonna resize')
+        self.etiqueta.pack()
+
+        self.button_aceptar = Button(master, text='Done', command=self.button_aceptar)
+        self.button_aceptar.pack()
+
+        self.button_cerrar = Button(master, text='Exit', command=master.quit)
+        self.button_cerrar.pack()
+
+        def button_aceptar(self):
+            print('Hey, wasup?')
